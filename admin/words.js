daa9ab52856a4029051c1fa46d99072a7aa3c537
Denny Sandner <sandnersoft@googlemail.com>
2020-08-09 11:14:13 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"abfahrtzeiten adapter settings": {
+		"en": "Adapter settings for abfahrtzeiten",
+		"de": "Adaptereinstellungen für abfahrtzeiten",
+		"ru": "Настройки адаптера для abfahrtzeiten",
+		"pt": "Configurações do adaptador para abfahrtzeiten",
+		"nl": "Adapterinstellingen voor abfahrtzeiten",
+		"fr": "Paramètres d'adaptateur pour abfahrtzeiten",
+		"it": "Impostazioni dell'adattatore per abfahrtzeiten",
+		"es": "Ajustes del adaptador para abfahrtzeiten",
+		"pl": "Ustawienia adaptera dla abfahrtzeiten",
+		"zh-cn": "abfahrtzeiten的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
